1922c6009047d3bf873c8861b6552f7304a25559
miholeus <me@miholeus.com>
2016-12-02 18:31:28 +0300

    form login and migration bundle

--- src/Soulex/CoreBundle/Controller/SecurityController.php
+++ src/Soulex/CoreBundle/Controller/SecurityController.php
@@ -3,11 +3,28 @@
 namespace Soulex\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('SoulexCoreBundle:security:login.html.twig');
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('soulex_core_homepage');
+        }
+
+        $form = $this->createForm('Soulex\CoreBundle\Form\Login');
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form->handleRequest($request);
+
+        return $this->render('SoulexCoreBundle:security:login.html.twig', [
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 }
