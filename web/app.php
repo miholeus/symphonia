7c6e2cd810c27892308c4a1889679b3ad0d66a3e
miholeus <me@miholeus.com>
2018-01-13 01:28:17 +0300

    one entry point :recycle:

--- web/app.php
+++ web/app.php
@@ -1,16 +1,24 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
 
 $env = getenv('SYMFONY_ENV') ?: 'prod';
-$kernel = new AppKernel($env, 'prod' !== $env);
-$kernel->loadClassCache();
+$debug = getenv('SYMFONY_DEBUG') === '1' && $env != 'prod';
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
