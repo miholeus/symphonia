962b96408aa54f0e3cac78fc337c62948541bab8
miholeus <me@miholeus.com>
2018-01-14 02:02:47 +0300

    add compiler to bundle :sparkles:

--- src/Symphonia/CoreBundle/SymphoniaCoreBundle.php
+++ src/Symphonia/CoreBundle/SymphoniaCoreBundle.php
@@ -3,7 +3,15 @@
 namespace Symphonia\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symphonia\CoreBundle\DependencyInjection\Compiler\DoctrineEntityListenerPass;
 
 class SymphoniaCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DoctrineEntityListenerPass());
+    }
 }
