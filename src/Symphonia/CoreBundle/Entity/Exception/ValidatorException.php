c1f4b05427fddf111e0eb66b8a4e10442ae5559b
miholeus <me@miholeus.com>
2018-01-14 02:02:19 +0300

    base exceptions :beers:

---
+++ src/Symphonia/CoreBundle/Entity/Exception/ValidatorException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Symphonia package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symphonia\CoreBundle\Entity\Exception;
+
+class ValidatorException extends \Symphonia\CoreBundle\Exception
+{
+
+}
